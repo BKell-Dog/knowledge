1afc7ed3017cb764cb61edaa54a44c3f862448b9
BKell-Dog <bkelldog59@gmail.com>
2025-12-14 01:04:52 -0500

    I forgot to delete the wrong code from last run. Correcting.

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -4,13 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-    Component.Header({
-      links: [
-        { title: "Home", url: "https://bkelldog.neocities.org/home/" }
-      ],
-    }),
-  ],
+  header: [],
   afterBody: [],
   footer: Component.Footer({
     links: {
